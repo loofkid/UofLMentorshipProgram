f1fc28c1485ae37045d7e46244038bd5894ca9f9
loofkid <loofkid@loofmail.com>
2021-10-27 19:04:46 -0400

    add get and post examples

--- Controllers/JobBoardController.cs
+++ Controllers/JobBoardController.cs
@@ -1,5 +1,9 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using UofLMentorshipProgram.Data;
+using UofLMentorshipProgram.Models.Data.Users;
 
 namespace Controllers
 {
@@ -12,6 +16,21 @@ namespace Controllers
         public JobBoardController(MentorshipContext mentorshipContext)
         {
             this.mentorshipContext = mentorshipContext;
+        }
+
+        [HttpGet("jobposting")]
+        public async Task<IActionResult> GetJobPostings()
+        {
+            return Ok(await mentorshipContext.JobPostings.Select(p => p).ToListAsync());
+        }
+
+        [HttpPost("jobposting")]
+        public async Task<IActionResult> AddJobPosting(SubmitModel input) {
+            var jobPosting = new JobPosting()// map submitmodel to jobposting
+            var jobPostingEntity = (await mentorshipContext.JobPostings.AddAsync(jobPosting)).Entity;
+            await mentorshipContext.SaveChangesAsync();
+
+            return CreatedAtAction(Url.Action($"jobposting/{jobPostingEntity.Id}"), jobPostingEntity);
         }
     }
 }
